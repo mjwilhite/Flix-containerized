0c6d122f2a60eec819bfd02de4ac494327e5b45c
MJ <mjwilhite1s@gmail.com>
2018-02-19 18:46:37 -0600

    FInish User Account

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
                     format: /\A\S+@\S+\z/,
                     uniqueness: { case_sensitive: false }
 
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    user && user.authenticate(password)
+  end
 end

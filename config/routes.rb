2ab4e2d283b71cb2fe99e547236b841694bb2e49
MJ <mjwilhite1s@gmail.com>
2018-02-19 18:17:33 -0600

    Remove Console from reviews/new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   root "movies#index"
   resources :movies do
   	resources :reviews
   end
+    resources :users
+    get 'signup' => 'users#new'
+
+    resource :session
 end

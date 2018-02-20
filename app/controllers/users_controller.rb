6672f60a3fa723ce311b19dc411c7dc395d4245a
MJ <mjwilhite1s@gmail.com>
2018-02-19 23:42:10 -0600

    Add Authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -22,11 +25,9 @@ class UsersController < ApplicationController
 end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -35,11 +36,10 @@ end
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to root_url, alert: "Account successfully deleted!"
-  end
+end
 
 private
 
@@ -48,3 +48,8 @@ private
       permit(:name, :email, :password, :password_confirmation)
     end
   end
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
+    end
+end

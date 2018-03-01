12ceedca4c37df2dd625ea41160cc49a87b61b84
MJ <mjwilhite1s@gmail.com>
2018-03-01 13:12:21 -0600

    Finish Through Associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :favorites
   root "movies#index"
   resources :movies do
   	resources :reviews
+    resources :favorites
   end
     resources :users
     get 'signup' => 'users#new'

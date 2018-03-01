ae113e0953954c8d7de8be93a571cee89c46d442
MJ <mjwilhite1s@gmail.com>
2018-03-01 16:28:55 -0600

    Add content to sidebar with content_for

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,7 @@ class MoviesController < ApplicationController
     if current_user
       @current_favorite = current_user.favorites.find_by(movie_id: @movie.id)
     end
+    @genres = @movie.genres
   end
 
   def edit
@@ -50,6 +51,6 @@ private
 
   def movie_params
     params.require(:movie).
-      permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name)
+      permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name, genre_ids: [])
   end
 end
